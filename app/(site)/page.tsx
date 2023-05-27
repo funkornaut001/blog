b7fb31db08c44d64af884bf60dd42fb32bc6425a
funkornaut001 <funkornaut@gmail.com>
2023-05-27 16:28:36 -0500

    yo 	modified:   app/(site)/layout.tsx 	modified:   app/(site)/page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,10 +8,10 @@ export default async function Home() {
   return (
     <div>
       <h1 className="text-7xl font-extrabold">Hello I&apos;m
-        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent"> Kapehe!</span>
+        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent"> Trevor!</span>
       </h1>
-      <p className="mt-3 text-xl text-gray-600">Aloha everyone! Check out my projects!</p>
-      <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
+      <p className="mt-3 text-xl text-gray-600">Gm, I am Trevor a self taught developer building blockchain applications, Chat-GPT powered tools, and learning about smart contract security</p>
+      <h2 className="mt-24 font-bold text-gray-700 text-3xl">Projects</h2>
 
       <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">{projects.map((project) => (
         <Link href={`/projects/${project.slug}`} key={project._id} className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition">
